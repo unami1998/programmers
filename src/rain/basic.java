d9b2e889f6a918f5949d66e19a22878df43d2c3f
lemon927_ <45076735+fpahs927@users.noreply.github.com>
2024-07-01 16:13:21 +0900

    rain 0703때 다시 풀기

---
+++ src/rain/basic.java
@@ -0,0 +1,10 @@
+package rain;
+
+public class basic {
+    public static void main(String[] args){
+        int a= 3;
+        for(int i=0; i<a; i++){
+            System.out.print("*");
+        }
+    }
+}
--- src/rain/rain.java
+++ src/rain/rain.java
@@ -3,14 +3,56 @@ package rain;
 import java.util.Scanner;
 
 public class rain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.print("빗물 가로 길이 배열 만들어보자");
         Scanner scanner = new Scanner(System.in);
         int height = scanner.nextInt();
         int width = scanner.nextInt();
-        int[][] land = new int[height][width];
-        System.out.print("land의 크기는 어떻게될까요?" + land.length + "그러면 width의 크기는" + land[0].length);
+        scanner.nextLine();
+        String[][] land = new String[height][width];
+        System.out.print("height 크기는 어떻게될까요?" + land.length + "그러면 width의 크기는" + land[0].length);
+
+        System.out.print("방벽을 입력해볼까?");
+        String input_barrier = scanner.nextLine().trim(); //양쪽 공백 제거
+        String[] parts = input_barrier.split("\\s+");
+        int[] barrier = new int[parts.length];
+
+        for (int i = 0; i < parts.length; i++) {
+            barrier[i] = Integer.parseInt(parts[i]);
+        }
+
+        //이제 barrier[i] = {3,0,1,4}가 된 상태다
+
+        for (int col = 0; col < barrier.length; col++) {
+            for (int j = 0; j < barrier[col]; j++) {
+                land[height - j-1][col] = "*";  //barrier=3
+            }
+        }
+
+
+        for (int i = 0; i < land.length; i++) {  //높이
+            for (int j = 0; j < land[i].length; j++) { //가
+                if (land[i][j] == null) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(land[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+
+        int count=0;
+        for (int H = 0; H < land.length; H++) {  //높이
+            for (int j = 0; j < land[H].length; j++) { //가
+                if (land[H][p-1].contains("*") && land[H]) {
+                    count++;
+                }
+            }
+        }
+        System.out.print("count" + count);
+
 
     }
 }
